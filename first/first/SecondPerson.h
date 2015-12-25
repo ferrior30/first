4f52d0ed245fd0678d931690fafb9df2b3f0011e
cweioo <jfgh431819@126.com>
2015-12-25 18:56:49 +0800

    add secondPerson

---
+++ first/first/SecondPerson.h
@@ -0,0 +1,13 @@
+//
+//  SecondPerson.h
+//  first
+//
+//  Created by ChenWei on 15/12/25.
+//  Copyright © 2015年 cw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SecondPerson : NSObject
+
+@end
---
+++ first/first/SecondPerson.m
@@ -0,0 +1,13 @@
+//
+//  SecondPerson.m
+//  first
+//
+//  Created by ChenWei on 15/12/25.
+//  Copyright © 2015年 cw. All rights reserved.
+//
+
+#import "SecondPerson.h"
+
+@implementation SecondPerson
+
+@end
