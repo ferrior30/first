a2d4e272addf56cdcb1a8b82bedc931e58608999
cweioo <jfgh431819@126.com>
2015-12-25 20:53:55 +0800

    add ThreePerson

---
+++ first/first/ThreePerson.h
@@ -0,0 +1,13 @@
+//
+//  ThreePerson.h
+//  first
+//
+//  Created by ChenWei on 15/12/25.
+//  Copyright © 2015年 cw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThreePerson : NSObject
+
+@end
---
+++ first/first/ThreePerson.m
@@ -0,0 +1,13 @@
+//
+//  ThreePerson.m
+//  first
+//
+//  Created by ChenWei on 15/12/25.
+//  Copyright © 2015年 cw. All rights reserved.
+//
+
+#import "ThreePerson.h"
+
+@implementation ThreePerson
+
+@end
